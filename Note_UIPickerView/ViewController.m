a9996e92f676fb9ddfc021139e1f2392953edf8f
nutletor <591809266@qq.com>
2016-01-10 20:49:31 +0800

    添加设置行视图代理方法，避免标题过长无法显示； 优化同时滚动两列组件crash的解决方案。

--- Note_UIPickerView/ViewController.m
+++ Note_UIPickerView/ViewController.m
@@ -34,7 +34,7 @@
     self.areaPickerView.dataSource = self;
     self.areaPickerView.delegate = self;
     
-    //将plist文件中的省市信息分别解析为省、市数组
+    //将plist文件中的'省/市'信息分别解析为'省/市'数组
     NSString * filePath = [[NSBundle mainBundle] pathForResource:@"areas" ofType:@"plist"];
     NSArray * areaAry = [NSArray arrayWithContentsOfFile:filePath];
     self.provinceAry = [NSMutableArray array];
@@ -54,18 +54,13 @@
     self.cityId = 61;
     self.provinceStr = @"北京市";
     self.cityStr = @"东城区";
-    //我的实际项目中采取的策略是，将用户选择的省市所匹配的id发送给服务器
+    //我的实际项目中采取的策略是，将用户选择的'省/市'所匹配的id发送给服务器
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-//- (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
-//{
-//    
-//}
 
 #pragma mark - UIPickerViewDataSource &Delegate
 //设置组件个数
@@ -78,14 +73,14 @@
 - (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
 {
     switch (component) {
-        case 0:
+        case 0://省
         {
             return self.provinceAry.count;
         }
             break;
-        case 1:
+        case 1://市
         {
-            //查找当前省对应的市的个数
+            //查找当前'省'对应的'市'的个数
             return [self.cityAry[self.provinceRow] count];
         }
             break;
@@ -95,31 +90,43 @@
     }
 }
 
-//设置组件标题（在任何组件滚动且即将显示之前未显示的行时便会调用该方法，类似tableview的重用机制）
+//<1>设置组件行视图（在任何组件滚动且即将显示之前未显示的行时便会调用该方法，类似tableview的重用机制）
+- (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(nullable UIView *)view
+{
+    UILabel* pickerLabel = (UILabel*)view;
+    if (!pickerLabel){
+        pickerLabel = [[UILabel alloc] init];
+        pickerLabel.textAlignment = NSTextAlignmentCenter;
+        pickerLabel.font = [UIFont systemFontOfSize:20];
+        pickerLabel.adjustsFontSizeToFitWidth = YES;//避免标题过长显示不全
+    }
+    
+    pickerLabel.text = [self pickerView:pickerView titleForRow:row forComponent:component];
+    return pickerLabel;
+}
+
+//<2>设置组件标题（如果同时实现了<1>的代理方法，便会优先调用<1>）
 - (nullable NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
 {
-    NSLog(@"%ld %ld", [pickerView selectedRowInComponent:0], [pickerView selectedRowInComponent:1]);
+//    NSLog(@"%ld %ld", [pickerView selectedRowInComponent:0], [pickerView selectedRowInComponent:1]);
     
     switch (component) {
-        case 0:
+        case 0://省
         {
             AreaModel * area = self.provinceAry[row];
             return area.name;
         }
             break;
-        case 1:
+        case 1://市
         {
+            /*
             //selectedRowInComponent所返回的值会随着组件滚动实时变化，并非仅在组件停止滚动后才会发生变化
             NSInteger selectedRow = [pickerView selectedRowInComponent:0];
-//            AreaModel * area = self.cityAry[selectedRow][row];
-            //以上代码，当同时滚动省市组件时，会造成不同行内容错乱，甚至如果滚动中实时变化的省对应的市的数量大于当前滚动中市的数量，还会造成越位crash
+            AreaModel * area = self.cityAry[selectedRow][row];
+            //以上代码，当同时滚动'省/市'组件时，会造成不同行内容错乱，甚至如果滚动中实时变化的'省'对应的'市'的数量大于当前滚动中'市'的数量，还会造成越位crash
+            */
             
-            //通过省组件停止滚动时保存的行数查找对应的市信息，便可避免越位crash
-            if (row >= [self.cityAry[self.provinceRow] count]) {
-                return @"";
-            }
-//            NSInteger count = [self.cityAry[self.provinceRow] count];
-//            NSInteger index = row < count ? row : count;
+            //通过'省'组件停止滚动时保存的行数查找对应的'市'信息，便可避免越位crash
             AreaModel * area = self.cityAry[self.provinceRow][row];
             return area.name;
         }
@@ -130,42 +137,28 @@
     }
 }
 
+//组件停止滚动时调用
 - (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
 {
     switch (component) {
-        case 0:
+        case 0://省
         {
             AreaModel * province = self.provinceAry[row];
             self.provinceId = province.idField;
             self.provinceStr = province.name;
             
-//            NSLog(@"%@", pickerView.subviews);
-//            for (UIGestureRecognizer * gesture in pickerView.gestureRecognizers) {
-//                [pickerView removeGestureRecognizer:gesture];
-////                gesture.cancelsTouchesInView = YES;
-//            }
+            //在'省'组件停止滚动时保存行数
+            self.provinceRow = [pickerView selectedRowInComponent:0];
             
             [pickerView reloadComponent:1];
             [pickerView selectRow:0 inComponent:1 animated:YES];
             AreaModel * city = self.cityAry[row][0];
             self.cityId = city.idField;
             self.cityStr = city.name;
-            
-            //在省组件停止滚动时保存行数
-            self.provinceRow = [pickerView selectedRowInComponent:0];
         }
             break;
-        case 1:
+        case 1://市
         {
-            NSInteger selectedRow = [pickerView selectedRowInComponent:0];
-//            AreaModel * city = self.cityAry[selectedRow][row];
-            //以上代码，同样可能造成越位crash
-            
-            if (row >= [self.cityAry[self.provinceRow] count]) {
-                return;
-            }
-//            NSInteger count = [self.cityAry[self.provinceRow] count];
-//            NSInteger index = row < count ? row : count;
             AreaModel * city = self.cityAry[self.provinceRow][row];
             self.cityId = city.idField;
             self.cityStr = city.name;
@@ -175,7 +168,5 @@
             break;
     }
 }
-
-//- (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(nullable UIView *)view
 
 @end
